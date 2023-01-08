3e1e48271eac36a60000ae19af5c538ec63ead07
suleimenova <suleimenova25@gmail.com>
2023-01-08 17:32:54 +0600

    added zip file

--- qa_guru_8_files/src/test/java/com/smira/FileTest.java
+++ qa_guru_8_files/src/test/java/com/smira/FileTest.java
@@ -1,18 +1,30 @@
 package com.smira;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVReader;
 import com.smira.model.Complex;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
 
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 
 public class FileTest {
+    ClassLoader cl = FileTest.class.getClassLoader();
+
     @DisplayName("Json check value")
     @Test
-
     void jsonTest() throws Exception {
         File file = new File("src/test/resources/complex.json");
         ObjectMapper objectMapper = new ObjectMapper();
@@ -25,4 +37,39 @@ public class FileTest {
         assertThat(complex.apartments.square).isEqualTo(40);
         assertThat(complex.apartments.floor).isEqualTo(2);
 
-}}
+    }
+
+    @DisplayName("Parsing zip file")
+    @Test
+    void zipTest() throws Exception {
+        ZipFile zf = new ZipFile(new File("src/test/resources/qa_guru_files.zip"));
+        try (ZipInputStream is = new ZipInputStream(cl.getResourceAsStream("qa_guru_files.zip"))){
+            ZipEntry entry;
+            while ((entry = is.getNextEntry()) != null) {
+                if (entry.getName() == "qa_guru_files.csv") {
+                    try (InputStream inputStream = zf.getInputStream(entry);
+                         CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                        List<String[]> content = reader.readAll();
+                        String[] row = content.get(1);
+                        assertThat(row[0]).isEqualTo("LastName");
+                    }
+                } else if (entry.getName() == "test_file.xlsx") {
+                    try (InputStream inputStream = zf.getInputStream(entry)) {
+                        XLS xls = new XLS(inputStream);
+                        assertThat(xls.excel.getSheetAt(0)
+                                .getRow(2)
+                                .getCell(1)
+                                .getStringCellValue()
+                        ).isEqualTo("Mara");
+                    }
+                } else if (entry.getName() == "test.pdf") {
+                    try (InputStream inputStream = zf.getInputStream(entry)) {
+                        PDF pdf = new PDF(inputStream);
+                        assertThat(pdf.author).contains("Sam Brannen");
+                    }
+                }
+            }
+        }
+    }
+}
+
