aad3ce4905e5952a756638869376de5ccaa184b8
suleimenova <suleimenova25@gmail.com>
2023-01-09 23:47:46 +0600

    add allure

--- qa_guru_9_jenkins/src/test/java/com/demoqa/pages/RegistrationFormPage.java
+++ qa_guru_9_jenkins/src/test/java/com/demoqa/pages/RegistrationFormPage.java
@@ -3,6 +3,7 @@ package com.demoqa.pages;
 import com.codeborne.selenide.SelenideElement;
 import com.demoqa.pages.components.CalendarComponent;
 import com.demoqa.pages.components.ResultsTableComponent;
+import io.qameta.allure.Step;
 
 import java.io.File;
 
@@ -21,6 +22,7 @@ public class RegistrationFormPage {
             lastNameInput = $("#lastName"),
             emailInput = $("#userEmail");
 
+    @Step("Открыть главную страницу")
     public RegistrationFormPage openPage() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text(TITLE_TEXT));
@@ -31,93 +33,97 @@ public class RegistrationFormPage {
         return this;
     }
 
+    @Step("Заполнить поле имя")
     public RegistrationFormPage setFirstName(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
 
+
     public RegistrationFormPage clearFirstName() {
         firstNameInput.clear();
 
         return this;
     }
-
+    @Step("Заполнить поле фамилия")
     public RegistrationFormPage setLastName(String value) {
         lastNameInput.setValue(value);
 
         return this;
     }
-
+    @Step("Ввести адрес электронной почты")
     public RegistrationFormPage setEmail(String value) {
         emailInput.setValue(value);
 
         return this;
     }
-
+    @Step("Выбрать пол")
     public RegistrationFormPage setGender(String value) {
         $("#genterWrapper").$(byText(value)).click();
 
         return this;
     }
-
+    @Step("Ввести номер телефона")
     public RegistrationFormPage setNumber(String value) {
         $("#userNumber").setValue(value);
 
         return this;
     }
-
+    @Step("Выбрать дату рождения")
     public RegistrationFormPage setBirthDate(String day, String month, String year) {
         $("#dateOfBirth-wrapper").click();
         calendarComponent.setDate(day, month, year);
 
         return this;
     }
+    @Step("Выбрать предмет")
     public RegistrationFormPage setSubjects(String value) {
         $("#subjectsInput").setValue(value).pressEnter();
 
         return this;
     }
-
+    @Step("Выбрать хобби")
     public RegistrationFormPage setHobbies(String value) {
         $("#hobbiesWrapper").$(byText(value)).click();
 
         return this;
-    }
+    }@Step("Загрузить картинку")
     public RegistrationFormPage uploadPicture(String path) {
         $("#uploadPicture").uploadFile(new File(path));
         return this;
     }
-
+    @Step("Заполнить адрес")
     public RegistrationFormPage setAddress(String address) {
         $("#currentAddress").setValue(address);
 
         return this;
     }
-
+    @Step("Выбрать штат")
     public RegistrationFormPage setState(String value) {
         $("#react-select-3-input").setValue(value).pressEnter();
 
         return this;
     }
-
+    @Step("Выбрать город")
     public RegistrationFormPage setCity(String value) {
         $("#react-select-4-input").setValue(value).pressEnter();
 
         return this;
     }
-
+    @Step("Кликаем на кнопку загрузить")
     public RegistrationFormPage clickSubmit() {
         $("#submit").click();
 
         return this;
     }
-
+    @Step("Проверяем, что открылась таблица результатов")
     public RegistrationFormPage checkResultsTableVisible() {
         resultsTableComponent.checkVisible();
 
         return this;
     }
+    @Step("Проверяем что поле {key} заполнено верно")
     public RegistrationFormPage checkResult(String key, String value){
         resultsTableComponent.checkResult(key, value);
 
--- qa_guru_9_jenkins/src/test/java/com/demoqa/tests/BaseTest.java
+++ qa_guru_9_jenkins/src/test/java/com/demoqa/tests/BaseTest.java
@@ -1,13 +1,22 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest {
     @BeforeAll
-    static void setup() {
+    static void configure() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+
+
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
-}
+    }
+
